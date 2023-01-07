659437ad01b59f06a89116f54cf247e25725b550
Vincent <vincent-dao@outlook.com>
2023-01-07 19:54:23 +0100

    merged

--- EJBank-ejb/src/main/java/com/ejbank/session/transaction/TransactionBeanLocal.java
+++ EJBank-ejb/src/main/java/com/ejbank/session/transaction/TransactionBeanLocal.java
@@ -7,7 +7,6 @@ public interface TransactionBeanLocal {
     TransactionResponsePayload submitTransaction(TransactionRequestPayload transactionRequestPayload);
     TransactionPreviewResponsePayload transactionPreview(int source, int dest, double amount, int author);
     TransactionListPayload listTransactions(int userId, int accountId, int offset);
-    TransactionPayload transactionPreview(int source, int dest, double amount, int author);
     Integer waitingValidation(int userId);
     TransactionValidationResponsePayload validationTransaction(TransactionValidationRequestPayload transactionValidationRequestPayload);
 }
